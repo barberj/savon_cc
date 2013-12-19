48b05838f078abc0e539080766863cdfff70c1f1
barberj <barber.justin@gmail.com>
2013-12-19 17:36:27 -0500

    updates for latest savon

--- lib/savon_cc.rb
+++ lib/savon_cc.rb
@@ -2,17 +2,17 @@ require "savon_cc/version"
 require "rexml/document"
 
 class Savon::Operation
-  alias_method :orig_create_response, :create_response
+  alias_method :orig_call, :call
 
-  def create_response response
-    stmp = Time.now.to_i.to_s
-    file_name = "#{instance_variable_get :@name}_#{stmp}.xml"
-    buffer = StringIO.new
-    xmldoc = REXML::Document.new response.body
-    xmldoc.write buffer, 1
-    File.write file_name, buffer.string
-
-    orig_create_response(response)
+  def call
+    orig_call.tap do |rsp|
+      stmp = Time.now.to_i.to_s
+      file_name = "#{instance_variable_get :@operation}_#{stmp}.xml"
+      buffer = StringIO.new
+      xmldoc = REXML::Document.new rsp.instance_variable_get(:@raw_response)
+      xmldoc.write buffer, 1
+      File.write file_name, buffer.string
+    end
   end
 end
 
