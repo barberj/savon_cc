6ee7c2f176382dcbd3cbf5eba2f67faa9dcb48cd
barberj <barber.justin@gmail.com>
2014-06-17 20:24:45 -0400

    savon 2.5

--- lib/savon_cc.rb
+++ lib/savon_cc.rb
@@ -1,15 +1,15 @@
 require "savon_cc/version"
 require "rexml/document"
 
-class Savon::Operation
+class Savon::Client
   alias_method :orig_call, :call
 
-  def call
-    orig_call.tap do |rsp|
+  def call(operation_name, locals={}, &block)
+    orig_call(operation_name, locals, &block).tap do |rsp|
       stmp = Time.now.to_i.to_s
-      file_name = "#{instance_variable_get :@operation}_#{stmp}.xml"
+      file_name = "#{operation_name}_#{stmp}.xml"
       buffer = StringIO.new
-      xmldoc = REXML::Document.new rsp.instance_variable_get(:@raw_response)
+      xmldoc = REXML::Document.new rsp.http.raw_body
       xmldoc.write buffer, 1
       File.write file_name, buffer.string
     end
